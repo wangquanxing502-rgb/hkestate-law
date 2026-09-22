c2532e8113fc889802e75830336cbb29360d3acb
wangquanxing502-rgb <wangquanxing502@gmail.com>
2026-09-22 17:21:28 +0800

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -30,6 +30,18 @@ function RootDocument({ children }: { children: React.ReactNode }) {
     <html lang="zh-Hant">
       <head>
         <HeadContent />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18259205570"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18259205570');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
